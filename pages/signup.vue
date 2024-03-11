8f6e290badaeafad60abaec11e17382f0db1070f
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-11 19:40:11 +0700

    New Page:  signup

--- pages/signup.vue
+++ pages/signup.vue
@@ -4,12 +4,12 @@
             <div class="d-table-cell">
                 <div class="container">
                     <div class="page-title-content">
-                        <h2>Sign In</h2>
+                        <h2>Sign Up</h2>
                         <ul>
                             <li>
                                 <NuxtLink to="/"> Home </NuxtLink>
                                 <i class="flaticon-fast-forward"></i>
-                                <p class="active"> Sign In </p>
+                                <p class="active"> Sign Up </p>
                             </li>
                         </ul>
                     </div>
@@ -23,8 +23,15 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6">
+                    <div class="sign-up-img">
+                        <div class="img">
+                            <img src="/images/signup.svg" alt="singup">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
                     <div class="signup-form">
-                        <h2>Welcome Back!</h2>
+                        <h2>Create your Account</h2>
                         <form>
                             <div class="row">
                                 <div class="col-lg-12">
@@ -34,23 +41,29 @@
                                 </div>
                                 <div class="col-lg-12">
                                     <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
                                         <input type="password" class="form-control" placeholder="Password">
                                     </div>
                                 </div>
                                 <div class="col-lg-12">
-                                    <NuxtLink to="/" class="box-btn">
-                                        Sign In
-                                    </NuxtLink>
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="checkme">
+                                        <label class="form-check-label" for="checkme">Remember me</label>
+                                    </div>
                                 </div>
-                                <span class="already">New to Rehab Hati! <NuxtLink to="/signup">Sign Up</NuxtLink>
+                                <div class="col-lg-12">
+                                    <a href="signin.html" class="box-btn">
+                                        Sign Up
+                                    </a>
+                                </div>
+                                <span class="already">Have already an account? <NuxtLink to="/login">Sign In</NuxtLink>
                                 </span>
                             </div>
                         </form>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="sign-up-img">
-                        <img src="/images/signup.svg" alt="singup">
                     </div>
                 </div>
             </div>
