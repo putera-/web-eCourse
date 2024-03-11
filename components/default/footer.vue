8fd71d3e0ec7433447e38cfc682cc7fb719c972a
Rizjami Putera <rizjamiputera@gmail.com>
2024-03-11 21:12:09 +0700

    - Update Contact Page - Update footer

---
+++ components/default/footer.vue
@@ -0,0 +1,258 @@
+<template>
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="footer-left">
+                        <a href="index.html" class="logo"><img src="/images/logo2.png" alt="logo" /></a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua.
+                        </p>
+                        <ul class="footer-social">
+                            <li>
+                                <a href="#">
+                                    <IconsFacebook style="width: 16px; height: 16px; fill:#BA8324" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <IconsTwitter style="width: 16px; height: 16px; fill:#BA8324" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <IconsEnvelope style="width: 16px; height: 16px; fill:#BA8324" />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <IconsGoogleplus style="width: 16px; height: 16px; fill:#BA8324" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-content fml-25">
+                        <h2>School</h2>
+                        <ul>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Support</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Career</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Teachers</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Staffs</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <div class="footer-content fml-15">
+                        <h2>Quick Links</h2>
+                        <ul>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Home</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Classes</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Courses</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> Why Us?</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> FAQ</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-content fml-15 fml-20">
+                        <h2>Find Us</h2>
+                        <ul>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> +1 (321) 984 754</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> +1 (281) 384 574</a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> <span class="__cf_email__"
+                                        data-cfemail="1179747d7d7e51747567783f727e7c">[email&#160;protected]</span></a>
+                            </li>
+                            <li>
+                                <a href="#"><i class="flaticon-next"></i> <span class="__cf_email__"
+                                        data-cfemail="e695939696899492a68382908fc885898b">[email&#160;protected]</span></a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="flaticon-next"></i> 28/A Street, New York, USA
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="copy-area">
+        <div class="container">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="copy">
+                        <p>
+                            {{ isDark ? 'DARK' : 'LIGHT' }}
+                            Copyright @RehabHatiofficial. All Rights Reserved by
+                            <a href="#" target="_blank"> Rehab Hati E-Course</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="switch-box">
+        <label id="switch" class="switch">
+            <input type="checkbox" @change="toggleDark()" id="slider">
+            <span class="slider round"></span>
+        </label>
+    </div>
+    <a href="#" class="scroll-top wow animate__animated animate__bounceInDown">
+        <IconsAngleDoubleUp style="width: 16px; height: 16px; fill:#fff" />
+    </a>
+</template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from "@vueuse/core";
+const isDark = useDark({
+    selector: "html",
+    attribute: "class",
+    valueDark: "theme-dark",
+    valueLight: "theme-light",
+});
+const toggleDark = useToggle(isDark);
+</script>
+
+<style scoped>
+.footer-area {
+    position: relative;
+    padding: 75px 0;
+    overflow: hidden;
+    background: url(/images/footer-area.png);
+    background-size: cover;
+}
+
+.footer-area .footer-left p {
+    color: #fff;
+    margin-top: 20px;
+}
+
+.footer-area .footer-left .footer-social {
+    margin: 0;
+    padding: 0;
+}
+
+.footer-area .footer-left .footer-social li {
+    display: inline-block;
+}
+
+.footer-area .footer-left .footer-social li a {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+    margin-right: 5px;
+    color: #BA8324;
+    border: 1px solid transparent;
+}
+
+.footer-area .footer-left .footer-social li a i {
+    font-size: 16px;
+}
+
+.footer-area .footer-left .footer-social li a:hover {
+    background: #BA8324;
+    color: #fff;
+    border: 1px dashed #fff;
+}
+
+.footer-area .footer-content h2 {
+    color: #fff;
+    font-size: 25px;
+}
+
+.footer-area .footer-content ul {
+    margin: 0;
+    padding: 0;
+}
+
+.footer-area .footer-content ul li a {
+    font-size: 16px;
+    color: #fff;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+    margin-top: 11px;
+    position: relative;
+}
+
+.footer-area .footer-content ul li a::before {
+    position: absolute;
+    content: "";
+    width: 0%;
+    height: 1px;
+    background: #000;
+    top: 13px;
+    left: 0;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+}
+
+.footer-area .footer-content ul li a i {
+    font-size: 9px;
+    margin-right: 2px;
+}
+
+.footer-area .footer-content ul li a:hover:before {
+    width: 100%;
+}
+
+.copy-area {
+    padding: 15px 0;
+    background: #000;
+    text-align: center;
+}
+
+.copy-area .copy {
+    margin: auto;
+}
+
+.copy-area .copy p {
+    color: #fff;
+    margin-bottom: 0;
+}
+
+.copy-area .copy a {
+    color: #fff;
+}
+
+.copy-area .copy a:hover {
+    color: #BA8324;
+}
+</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -7,7 +7,7 @@
                         <h2>Contact Us</h2>
                         <ul>
                             <li>
-                                <a href="index.html"> Home </a>
+                                <NuxtLink to="/"> Home </NuxtLink>
                                 <i class="flaticon-fast-forward"></i>
                                 <p class="active">Contact</p>
                             </li>
@@ -17,10 +17,291 @@
             </div>
         </div>
     </div>
+
+    <div class="contact-service">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="single-service text-center">
+                        <div class="service-icon">
+                            <IconsClock />
+                        </div>
+                        <div class="service-content">
+                            <p>Sun - Thu : 10:00AM - 06:00PM</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <div class="single-service text-center">
+                        <div class="service-icon">
+                            <IconsLocation />
+                        </div>
+                        <div class="service-content">
+                            <p>28/A Street, New York City, USA</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-sm-12">
+                    <div class="single-service text-center sst-10">
+                        <div class="service-icon">
+                            <IconsPhone />
+                        </div>
+                        <div class="service-content">
+                            <p>+1 (321) 984 754</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="home-contact-area pb-100">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-lg-6 ps-0">
+                    <div class="contact-img">
+                        <img src="/images/contact.png" alt="contact" />
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="home-contact-content">
+                        <h2>What Do You Want to Know?</h2>
+                        <form id="contactForm">
+                            <div class="row">
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" name="name" id="name" class="form-control" required
+                                            data-error="Please enter your name" placeholder="Your Name" />
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="email" name="email" id="email" class="form-control" required
+                                            data-error="Please enter your email" placeholder="Your Email" />
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" name="phone_number" id="phone_number" required
+                                            data-error="Please enter your number" class="form-control"
+                                            placeholder="Your Phone" />
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-sm-6">
+                                    <div class="form-group">
+                                        <input type="text" name="msg_subject" id="msg_subject" class="form-control"
+                                            required data-error="Please enter your subject"
+                                            placeholder="Your Subject" />
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12">
+                                    <div class="form-group">
+                                        <textarea name="message" class="form-control" id="message" cols="30" rows="5"
+                                            required data-error="Write your message"
+                                            placeholder="Your Message"></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12">
+                                    <button type="submit" class="default-btn page-btn box-btn">
+                                        Send Message
+                                    </button>
+                                    <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.contact {
+    position: relative;
+    position: relative;
+    background-size: cover;
+    background-repeat: no-repeat;
+    z-index: 1;
+    background: url(/images/contact-bg1.png);
+    height: 400px;
+    background-size: cover;
+    background-position: center center;
+    z-index: 2;
+    background-attachment: fixed;
+}
+
+.contact::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background: url(/images/banner-overly.png);
+    left: 0;
+    top: 0;
+    background-size: cover;
+}
+
+.contact-service {
+    position: relative;
+    z-index: 1;
+    padding: 75px 0;
+}
+
+.contact-service .single-service {
+    position: relative;
+    -webkit-box-shadow: 2px 2px 20px 4px rgba(0, 0, 0, 0.07);
+    box-shadow: 2px 2px 20px 4px rgba(0, 0, 0, 0.07);
+    padding: 65px 20px;
+    background: url(/images/servicebg.png) center center;
+    border-radius: 10px;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+    background-position: center center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    height: 100%;
+}
+
+.contact-service .single-service .service-icon {
+    width: 45px;
+    height: 45px;
+    background-color: #BA8324;
+    color: #fff;
+    -webkit-transition: 0.3s;
+    transition: 0.3s;
+    border-radius: 50%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.contact-service .single-service .service-icon svg {
+    width: 24px;
+    height: 24px;
+    fill: #fff
+}
+
+.contact-service .single-service .service-content p {
+    margin-bottom: 0;
+    font-size: 20px;
+    color: #4f4f4f;
+    margin-top: 10px;
+}
+
+.contact-service .single-service:hover {
+    -webkit-transform: translateY(-15px);
+    transform: translateY(-15px);
+}
+
+::-webkit-input-placeholder {
+    color: #bdbdbd;
+    opacity: 1;
+}
+
+:-ms-input-placeholder {
+    color: #bdbdbd;
+    opacity: 1;
+}
+
+::-ms-input-placeholder {
+    color: #bdbdbd;
+    opacity: 1;
+}
+
+::placeholder {
+    color: #bdbdbd;
+    opacity: 1;
+}
+
+.home-contact-area {
+    position: relative;
+    overflow: hidden;
+}
+
+.home-contact-area .text-danger {
+    color: #dc3545 !important;
+    margin-bottom: 0 !important;
+    margin-top: 10px !important;
+}
+
+.home-contact-area .text-success {
+    color: #28a745 !important;
+}
+
+.home-contact-area .home-contact-content {
+    position: relative;
+    background: url(/images/contact-bg.png);
+    background-size: cover;
+    padding: 50px;
+    border-radius: 40px;
+    left: -20%;
+    -webkit-box-shadow: 0 1px 10px 5px rgba(228, 221, 221, 0.363);
+    box-shadow: 0 1px 10px 5px rgba(228, 221, 221, 0.363);
+}
+
+.home-contact-area .home-contact-content h2 {
+    font-family: "Playfair Display", serif;
+    font-size: 35px;
+    margin-bottom: 30px;
+    font-weight: bold;
+}
+
+.home-contact-area .home-contact-content .form-group {
+    margin-bottom: 15px;
+}
+
+.home-contact-area .home-contact-content input.form-control {
+    height: 60px;
+    border-radius: 90px;
+    border: 1px solid #c0ccff;
+    padding-left: 20px;
+}
+
+.home-contact-area .home-contact-content textarea.form-control {
+    width: 100%;
+    border-radius: 15px;
+    border: 1px solid #c0ccff;
+    height: 170px;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+    padding-left: 20px;
+    padding-top: 15px;
+}
+
+.home-contact-area .home-contact-content .form-control:focus {
+    -webkit-box-shadow: 0 0 0 0.04rem #BA8324;
+    box-shadow: 0 0 0 0.04rem #BA8324;
+}
+
+.home-contact-area .box-btn {
+    background: #BA8324;
+    color: #fff;
+    padding: 18px 25px;
+    border: 1px solid transparent;
+}
+
+.home-contact-area .box-btn:hover {
+    border: 1px dashed #BA8324;
+    background: #fff;
+    color: #BA8324;
+}
+
+.home-contact-area .list-unstyled {
+    margin-bottom: 0;
+    color: #dc3545;
+}
+</style>
